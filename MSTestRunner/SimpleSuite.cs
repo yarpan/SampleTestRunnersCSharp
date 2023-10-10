93f75a4c4a0508d26c5d4d7e1dfe861e5231e7ca
Yaroslav Panchishyn <yarpan@gmail.com>
2023-10-10 19:37:36 +0300

    mstest add

---
+++ MSTestRunner/SimpleSuite.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTestRunner
+{
+    [TestClass]
+    public class SimpleSuite
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [ClassInitialize]
+        public static void BeforeClass(TestContext a)
+        {
+            Console.WriteLine("BeforeClass");
+        }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            Console.WriteLine("BeforeTest");
+        }
+
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
+
+        [TestMethod]
+        public void Test2()
+
+        {
+            Console.WriteLine("Test2");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Console.WriteLine("Test3");
+        }
+
+
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("AfterTest");
+        }
+
+        [ClassCleanup]
+        public static void AfterClass()
+        {
+            Console.WriteLine("AfterClass");
+        }
+
+    }
+}
+
---
+++ MSTestRunner/SimpleTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTestRunner
+{
+    [TestClass]
+    public class SimpleTest
+    {
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            Console.WriteLine("BeforeTest");
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
+
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("AfterTest");
+        }
+
+    }
+}
+
