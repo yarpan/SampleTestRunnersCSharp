7164c565c1c466a8ff633cd451c47b6d004764e4
Yaroslav Panchishyn <yarpan@gmail.com>
2023-10-12 20:23:52 +0300

    mstest update

---
+++ MSTestParallelRunner/TestSuite1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//[assembly: Parallelize(Workers = 3, Scope = ExecutionScope.ClassLevel)]
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.MethodLevel)]
+
+
+
+namespace MSTestParallelRunner
+{
+    [TestClass]
+    public class TestSuite1
+    {
+
+        [ClassInitialize]
+        public static void BeforeClass(TestContext a)
+        {
+            Console.WriteLine("BeforeClass");
+        }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            Console.WriteLine("BeforeTest");
+        }
+
+
+
+        [TestMethod]
+        [DoNotParallelize]
+        public void Test1()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Test1");
+        }
+
+        [TestMethod]
+        [DoNotParallelize]
+        public void Test2()
+
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Test1");
+        }
+
+        [TestMethod]
+        [DoNotParallelize]
+        public void Test3()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Test1");
+        }
+
+
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("AfterTest");
+        }
+
+        [ClassCleanup]
+        public static void AfterClass()
+        {
+            Console.WriteLine("AfterClass");
+        }
+
+    }
+}
---
+++ MSTestParallelRunner/TestSuite3.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MSTestParallelRunner
+{
+    [TestClass]
+    public class TestSuite3
+    {
+
+        [ClassInitialize]
+        public static void BeforeClass(TestContext a)
+        {
+            Console.WriteLine("BeforeClass");
+        }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            Console.WriteLine("BeforeTest");
+        }
+
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            Thread.Sleep(2000); 
+            Console.WriteLine("Test1");
+        }
+
+        [TestMethod]
+        public void Test2()
+
+        {
+            Thread.Sleep(2000); 
+            Console.WriteLine("Test2");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Thread.Sleep(2000); 
+            Console.WriteLine("Test3");
+        }
+
+
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("AfterTest");
+        }
+
+        [ClassCleanup]
+        public static void AfterClass()
+        {
+            Console.WriteLine("AfterClass");
+        }
+
+    }
+}
